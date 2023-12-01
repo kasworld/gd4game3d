0d26b999203a551e990a77ec942c93f28b54bfa3
SeukWon Kang <kasworld@gmail.com>
2023-12-01 10:11:08 +0900

    rollback bounce

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,17 +1,25 @@
-class_name Ball extends BounceNode3D
+class_name Ball extends Node3D
 
 const SPEED_LIMIT :float = 100
 var alive :bool
 var life_start :float
 
+var velocity :Vector3
+var bounce_area :AABB
+var radius :float
+
+
 func init(ba :AABB, p :Vector3)->void:
-	area = ba
 	alive = true
 	life_start = Time.get_unix_time_from_system()
+
+	bounce_area = ba
 	position = p
 	radius = $Sphere.radius
 	velocity = Vector3( (randf()-0.5)*SPEED_LIMIT,(randf()-0.5)*SPEED_LIMIT,(randf()-0.5)*SPEED_LIMIT)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
-	bounce()
+	var bn = Bounce.bounce3d(position,velocity,bounce_area,radius)
+	position = bn.position
+	velocity = bn.velocity
--- moving_camera/moving_camera.gd
+++ moving_camera/moving_camera.gd
@@ -1,19 +1,26 @@
-extends BounceNode3D
+extends Node3D
 
 const SPEED_LIMIT :float = 100
 var life_start :float
 var dest_node3d :Node3D
 
+var velocity :Vector3
+var bounce_area :AABB
+var radius :float
+
 func init(ba :AABB, p :Vector3, dst :Node3D)->void:
-	area = ba
 	life_start = Time.get_unix_time_from_system()
+	dest_node3d = dst
+
+	bounce_area = ba
 	position = p
 	velocity = Vector3( (randf()-0.5)*SPEED_LIMIT,(randf()-0.5)*SPEED_LIMIT,(randf()-0.5)*SPEED_LIMIT)
-	dest_node3d = dst
 
 func _process(delta: float) -> void:
 	$Camera3D.look_at(dest_node3d.position)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
-	bounce()
+	var bn = Bounce.bounce3d(position,velocity,bounce_area,radius)
+	position = bn.position
+	velocity = bn.velocity
