136f5edf3d1e8d44eebbe6a465174ed1e6587188
SeukWon Kang <kasworld@gmail.com>
2023-12-01 16:54:03 +0900

    many ball

--- main.gd
+++ main.gd
@@ -1,8 +1,22 @@
 extends Node3D
 
+var ball_scene = preload("res://ball/ball.tscn")
+
+const BALL_COUNT = 1000
+var ball_list = []
+var b_box :AABB
 func _ready() -> void:
 	$DirectionalLight3D.look_at(Vector3.ZERO)
-	var b_box = AABB( $BoundBox.position -$BoundBox.size/2, $BoundBox.size)
-	$Ball.init( b_box, Vector3.ZERO )
-	$MovingCamera.init( b_box, Vector3.ZERO, $Ball )
+	b_box = AABB( $BoundBox.position -$BoundBox.size/2, $BoundBox.size)
+	add_ball()
+	$MovingCamera.init( b_box, Vector3.ZERO, ball_list[0] )
 
+func add_ball()->void:
+	var ball = ball_scene.instantiate()
+	ball.init( b_box, Vector3.ZERO )
+	ball_list.append(ball)
+	add_child(ball)
+
+func _process(delta: float) -> void:
+	if ball_list.size() < BALL_COUNT:
+		add_ball()
