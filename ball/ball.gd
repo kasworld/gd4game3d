021a49ede8296fb65442c499a08599c4d855ef64
SeukWon Kang <kasworld@gmail.com>
2023-12-22 18:38:41 +0900

    ball color changed on bounce

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,25 +2,36 @@ class_name Ball extends Node3D
 
 var velocity :Vector3
 var bounce_area :AABB
-var radius :float
+var radius :float = 0.5
 var speed_max :float
 var speed_min :float
 var sphere_count :int
 var sphere_list = []
 var sphere_cursor :int
+var color_mat_list = []
+var current_mat :Material
 
-func init(ba :AABB, count :int, mesh :Mesh)->void:
+func init(ba :AABB, count :int, comats :Array)->void:
+	color_mat_list = comats
 	bounce_area = ba
 	sphere_count = count
-	radius = mesh.radius
 	speed_max = radius * 300
 	speed_min = radius * 120
 	velocity = Vector3( (randf()-0.5)*speed_max,(randf()-0.5)*speed_max,(randf()-0.5)*speed_max)
+	current_mat = color_mat_list.pick_random()
 	for i in sphere_count:
 		var sp = MeshInstance3D.new()
-		sp.mesh = mesh
+		sp.mesh = new_mesh(radius,current_mat)
 		add_child(sp)
 		sphere_list.append(sp)
+
+func new_mesh(r :float, mat :Material)->Mesh:
+	var mesh = SphereMesh.new()
+	mesh.radius = r
+	#mesh.radial_segments = 100
+	#mesh.rings = 100
+	mesh.material = mat
+	return mesh
 
 func move(delta :float)->void:
 	var old_sphere = sphere_list[sphere_cursor%sphere_count]
@@ -33,10 +44,16 @@ func move_sphere(delta: float, sp :Node3D) -> void:
 	var bn = Bounce.bounce3d(sp.position,velocity,bounce_area,radius)
 	sp.position = bn.position
 	velocity = bn.velocity
+	var bounced = false
 	for i in 3:
 		# change vel on bounce
 		if bn.bounced[i] != 0 :
 			velocity[i] = -random_positive(speed_max/2)*bn.bounced[i]
+			bounced = true
+
+	if bounced :
+		current_mat = color_mat_list.pick_random()
+	sp.mesh.material = current_mat
 
 	if velocity.length() > speed_max:
 		velocity = velocity.normalized() * speed_max
--- main.gd
+++ main.gd
@@ -4,29 +4,26 @@ var ball_scene = preload("res://ball/ball.tscn")
 
 const BALL_COUNT = 10
 const TAIL_COUNT = 50
-const RADIUS = 0.5
 var ball_list = []
 var b_box :AABB
+var color_mat_list = []
+
 func _ready() -> void:
 	$DirectionalLight3D.position = $BoundBox.size *0.45
 	$DirectionalLight3D.look_at(Vector3.ZERO)
 	b_box = AABB( $BoundBox.position -$BoundBox.size/2, $BoundBox.size)
-	add_ball(TAIL_COUNT, NamedColorList.color_list.pick_random()[0])
+	for i in NamedColorList.color_list:
+		var co = i[0]
+		color_mat_list.append(new_mat(co))
+
+	add_ball(TAIL_COUNT)
 	$MovingCamera.init( b_box, Vector3.ZERO, ball_list[0] )
 
-func add_ball(tc :int, co :Color)->void:
+func add_ball(tc :int)->void:
 	var ball = ball_scene.instantiate()
-	ball.init( b_box, tc, new_mesh(RADIUS, co) )
+	ball.init( b_box, tc, color_mat_list)
 	ball_list.append(ball)
 	add_child(ball)
-
-func new_mesh(r :float, co: Color)->Mesh:
-	var mesh = SphereMesh.new()
-	mesh.radius = r
-	#mesh.radial_segments = 100
-	#mesh.rings = 100
-	mesh.material = new_mat(co)
-	return mesh
 
 func new_mat(co: Color)->Material:
 	var mat = StandardMaterial3D.new()
@@ -37,9 +34,9 @@ func new_mat(co: Color)->Material:
 
 func _process(delta: float) -> void:
 	if ball_list.size() < BALL_COUNT:
-		add_ball(TAIL_COUNT,  NamedColorList.color_list.pick_random()[0])
+		add_ball(TAIL_COUNT)
 
-	$LabelInfo.text = "Ball %d\n(%.1f,%.1f,%.1f)\n%.1fFPS" % [
-		ball_list.size(),
+	$LabelInfo.text = "Ball %dx%d\n(%.1f,%.1f,%.1f)\n%.1fFPS" % [
+		ball_list.size(),TAIL_COUNT,
 		$MovingCamera.position.x, $MovingCamera.position.y, $MovingCamera.position.z,
 		1.0/delta]
