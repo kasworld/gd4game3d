7b8b13fde19d8a444b396a435b90023710885df3
SeukWon Kang <kasworld@gmail.com>
2023-12-23 12:41:04 +0900

    torus rotate

--- ball/ball.gd
+++ ball/ball.gd
@@ -10,6 +10,8 @@ var sphere_list = []
 var sphere_cursor :int
 var color_mat_list = []
 var current_mat :Material
+var current_rot :Vector3
+var current_rot_accel :Vector3
 
 func init(ba :AABB, count :int, comats :Array)->void:
 	color_mat_list = comats
@@ -21,15 +23,35 @@ func init(ba :AABB, count :int, comats :Array)->void:
 	current_mat = color_mat_list.pick_random()
 	for i in sphere_count:
 		var sp = MeshInstance3D.new()
-		sp.mesh = new_mesh(radius,current_mat)
+		sp.mesh = new_torus(radius,current_mat)
 		add_child(sp)
 		sphere_list.append(sp)
 
-func new_mesh(r :float, mat :Material)->Mesh:
+func new_sphere(r :float, mat :Material)->Mesh:
 	var mesh = SphereMesh.new()
 	mesh.radius = r
 	#mesh.radial_segments = 100
 	#mesh.rings = 100
+	mesh.material = mat
+	return mesh
+
+func new_box(r :float, mat :Material)->Mesh:
+	var mesh = BoxMesh.new()
+	mesh.size = Vector3(r,r,r)
+	mesh.material = mat
+	return mesh
+
+func new_text(r :float, mat :Material)->Mesh:
+	var mesh = TextMesh.new()
+	mesh.font_size = r*500
+	mesh.text = "A"
+	mesh.material = mat
+	return mesh
+
+func new_torus(r :float, mat :Material)->Mesh:
+	var mesh = TorusMesh.new()
+	mesh.inner_radius = r/2
+	mesh.outer_radius = r
 	mesh.material = mat
 	return mesh
 
@@ -53,12 +75,18 @@ func move_sphere(delta: float, sp :Node3D) -> void:
 
 	if bounced :
 		current_mat = color_mat_list.pick_random()
+		current_rot_accel = Vector3(rand_rad(),rand_rad(),rand_rad())
 	sp.mesh.material = current_mat
+	current_rot += current_rot_accel
+	sp.rotation = current_rot
 
 	if velocity.length() > speed_max:
 		velocity = velocity.normalized() * speed_max
 	if velocity.length() < speed_min:
 		velocity = velocity.normalized() * speed_min
+
+func rand_rad()->float:
+	return randf_range(-PI,PI)/10
 
 func random_positive(w :float)->float:
 	return randf_range(w/10,w)
